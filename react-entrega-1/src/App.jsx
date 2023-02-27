58c10df43cb953553d5a35297ec1394619ec1893
Guerrero Matias <guerrerom@fecovita.com>
2023-02-27 09:54:08 -0300

    UPD2702231000AM

--- react-entrega-1/src/App.jsx
+++ react-entrega-1/src/App.jsx
@@ -1,3 +1,4 @@
+import NavBar from "./Components/NavBar"
 function App() {
   return (
     <div>
---
+++ react-entrega-1/src/Components/NavBar/index.jsx
@@ -0,0 +1,10 @@
+import Lista from "../ItemListContainer";
+function NavBar() {
+    return (
+        <div>        
+            <Lista nombre="Productos"/>
+
+        </div>
+    )
+}
+export default NavBar
